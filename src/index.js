43472b069825b8b4eff967a04822e10564af43f7
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-20 16:42:41 +0530

    Created CRUD operations in Repository layer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const Tweet = require('./models/tweet');
+const TweetRepository = require('./repository/tweet-repository');
 
 app.listen(3000, async () => {
     console.log("Server started on PORT: 3000 ");
@@ -13,9 +13,8 @@ app.listen(3000, async () => {
     //     userEmail: 'a@b.com'
     // });
     //  const tweets = await Tweet.find({ userEmail: 'a@b.com' });
-    const tweet = await Tweet.findById('65d47cd5c7289c4e9d93e77b');
-    tweet.userEmail = 'Changed_email@vb.com';
-    await tweet.save();
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.update('65d47cd5c7289c4e9d93e77b', { content: 'The updated Content' });
     console.log(tweet);
 })
 
