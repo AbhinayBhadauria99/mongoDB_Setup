2b686424f2ff1bfcc67df147c55416f887f6b65e
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-20 15:33:39 +0530

    Connected MongoDB by using mongoose

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const connect = require('./config/database');
+const app = express();
+
+app.listen(3000, async () => {
+    console.log("Server started on PORT: 3000 ");
+    await connect();
+    console.log("MondoDB connected");
+})
+
+
