ea89949aeaa6c94fb03c21d75210e7129e76cd9f
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-20 16:11:23 +0530

    To fetch all the data we use Tweets.find()

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ app.listen(3000, async () => {
     console.log("Server started on PORT: 3000 ");
     await connect();
     console.log("MondoDB connected");
-    const tweet = await Tweet.create({
-        content: 'Second tweet',
-        userEmail: 'a@b.com'
-    });
-    console.log(tweet);
+    // const tweet = await Tweet.create({
+    //     content: 'Second tweet',
+    //     userEmail: 'a@b.com'
+    // });
+    const tweets = await Tweet.find({ userEmail: 'a@b.com' });
+    console.log(tweets);
 })
 
 
