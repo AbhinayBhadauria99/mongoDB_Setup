11ac758d16ee17eaa342d44295241e8e50435def
Abhinay S. Bhadauria <knowledgepoint887@gmail.com>
2024-02-22 09:20:20 +0530

    Added getAll function with offset and limit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.listen(3000, async () => {
     // });
     //  const tweets = await Tweet.find({ userEmail: 'a@b.com' });
     const tweetRepo = new TweetRepository();
-    const tweet = await tweetRepo.getWithComments('65d4aefdc5347386d3f0a30f');
+    const tweet = await tweetRepo.getAll(1, 2);
     console.log(tweet);
 })
 
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -20,7 +20,7 @@ class TweetRepository {
 
     async getWithComments(id) {
         try {
-            const tweet = await Tweet.findById(id).populate({ path: 'comments' });
+            const tweet = await Tweet.findById(id).populate({ path: 'comments' }).lean();  //lean converts response from mongoose object to JS Object
             return tweet;
         } catch (error) {
             console.log(error);
@@ -39,6 +39,15 @@ class TweetRepository {
     async destroy(id) {
         try {
             const tweet = await Tweet.findByIdAndRemove(id);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getAll(offset, limit) {
+        try {
+            const tweet = await Tweet.find().skip(offset).limit(limit); //offset will skip first two tweet and limit will tell how much tweets to display
             return tweet;
         } catch (error) {
             console.log(error);
